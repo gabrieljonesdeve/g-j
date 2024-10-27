b878920a2452ea2aeb53923b826dbf2585c5de64
Gabriel Jones <gabriel.jones@mail.urp.gjdev.it>
2024-10-27 23:56:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
 
+    fetch('head.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('head').innerHTML = data;
+        });
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
